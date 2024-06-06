632b3387ff077b7837082dfe996a7566ed98c26d
John <john.l.hansen@gmail.com>
2024-06-05 17:39:30 -0700

    break up strip content and update layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,32 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-      <div className="">
+      <div className="flex flex-wrap md:flex-nowrap w-full">
         <Image
-          className="relative dark:invert"
-          src="/rovers-call.png"
+          className="flex-1 md:w-1/3 p-1 max-w-full relative dark:invert"
+          src="/rovers-call-01.webp"
+          alt="Rover's Call"
+          width={2000}
+          height={1000}
+          priority
+        />
+        <Image
+          className="flex-1 md:w-1/3 p-1 max-w-full relative dark:invert"
+          src="/rovers-call-02.webp"
+          alt="Rover's Call"
+          width={2000}
+          height={1000}
+          priority
+        />
+        <Image
+          className="flex-1 md:w-1/3 p-1 max-w-full relative dark:invert"
+          src="/rovers-call-03.webp"
           alt="Rover's Call"
           width={2000}
           height={1000}
           priority
         />
       </div>
-
     </main>
   );
 }
